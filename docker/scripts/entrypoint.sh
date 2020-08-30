9a47d61ca2ba8a6b4ba38590685d39dc47d7bdc1
Thales Alves <thales@thalesalv.es>
2020-08-30 00:16:36 -0300

    adicionado Dockerfile de openshift

--- docker/scripts/entrypoint.sh
+++ docker/scripts/entrypoint.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-nohup java -Dspring.profiles.active="$APP_PROFILE" -jar /opt/simplecrud-spring/target/simplecrud-0.0.1-SNAPSHOT.jar > java_log &
-tail -f java_log
+java -Dspring.profiles.active="$APP_PROFILE" -jar /opt/simplecrud-spring/target/simplecrud-0.0.1-SNAPSHOT.jar 
