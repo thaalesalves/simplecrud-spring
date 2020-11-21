9212761b9fbe9d0e89d943fd199a6c45072b887c
Thales Alves <thales@thalesalv.es>
2020-11-21 15:31:03 -0300

    correção de instalação do java manual

--- docker/scripts/entrypoint.sh
+++ docker/scripts/entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-java -Dspring.profiles.active="$APP_PROFILE" -jar /opt/simplecrud-spring/target/simplecrud-0.0.1-SNAPSHOT.jar 
+$JAVA_HOME/bin/java -Dspring.profiles.active="$APP_PROFILE" -jar /opt/simplecrud-spring/target/simplecrud-0.0.1-SNAPSHOT.jar 
